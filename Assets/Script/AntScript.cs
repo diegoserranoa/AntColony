7454031ac26ea9074ba3b7f77064839c8e38a1b3
Diego Serrano <mail@diegoserranoa.com>
2014-05-02 02:15:11 -0500

    First commit

---
+++ Assets/Script/AntScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AntScript : MonoBehaviour {
+
+	int maxX = 100;
+	int maxY = 100;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log (this.transform.position.x);
+		Debug.Log (maxX);
+		if (this.transform.position.x < maxX) {
+			//transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+		}
+
+		if (this.transform.position.y < maxY) {
+			//transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+		}
+	}
+
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		Debug.Log("Collision");
+	}
+}
---
+++ Assets/Script/TerrainScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TerrainScript : MonoBehaviour {
+
+	public Transform prefab;
+	float time;
+
+	int foodGenerationInterval = 100;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;	
+		if (time > 1) {
+			Debug.Log("Food");
+			time = 0;
+			// Generate food
+			int x = Random.Range(0,100);
+			int y = Random.Range(0,100);
+			Transform food = Instantiate(prefab) as Transform;
+			food.transform.position = new Vector2(x, y);
+		}
+	}
+}
