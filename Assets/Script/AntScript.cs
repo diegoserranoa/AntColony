615342c93a54ab2c3dc406cbaf281dbee95fa9cf
Diego Serrano <mail@diegoserranoa.com>
2014-05-02 03:53:10 -0500

    Im dead

--- Assets/Script/AntScript.cs
+++ Assets/Script/AntScript.cs
@@ -5,6 +5,8 @@ public class AntScript : MonoBehaviour {
 
 	int maxX = 100;
 	int maxY = 100;
+	bool foundSomething = false;
+
 
 	// Use this for initialization
 	void Start () {
@@ -13,18 +15,25 @@ public class AntScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log (this.transform.position.x);
-		Debug.Log (maxX);
-		if (this.transform.position.x < maxX) {
-			//transform.position = new Vector2(transform.position.x + 1, transform.position.y);
-		}
+		if (foundSomething) {
+			Debug.Log ("Found food.");
+			foundSomething = false;
 
-		if (this.transform.position.y < maxY) {
-			//transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+
+		} else {
+			if (this.transform.position.x < maxX) {
+				//transform.position = new Vector2(transform.position.x + 1, transform.position.y);
+			}
+			
+			if (this.transform.position.y < maxY) {
+				//transform.position = new Vector2(transform.position.x, transform.position.y + 1);
+			}
 		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collision) {
-		Debug.Log("CollisionTrigger");
+		Debug.Log(collision);
+
+		foundSomething = true;
 	}
 }
